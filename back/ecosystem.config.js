f158ab37e99e590ff298af662ef2e4ac58a32941
jasony62 <jasony62@qq.com>
2019-07-09 16:04:11 +0800

    update code

--- back/ecosystem.config.js
+++ back/ecosystem.config.js
@@ -1,21 +1,19 @@
 module.exports = {
-  apps: [
-    {
-      name: "teamprism-ue",
-      script: "./bin/www",
+  apps: [{
+    name: "teamprism-ue",
+    script: "./bin/www",
 
-      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-      instances: 1,
-      autorestart: true,
-      watch: true,
-      ignore_watch: ["node_modules"],
-      max_memory_restart: "1G",
-      env: {
-        NODE_ENV: "development"
-      },
-      env_production: {
-        NODE_ENV: "production"
-      }
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    instances: 1,
+    autorestart: true,
+    watch: true,
+    ignore_watch: ["node_modules"],
+    max_memory_restart: "1G",
+    env: {
+      NODE_ENV: "development"
+    },
+    env_production: {
+      NODE_ENV: "production"
     }
-  ]
+  }]
 }
