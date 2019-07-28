7dba7bfa90e86a0fe07e6db97857d87c8e1407c0
jasony62 <jasony62@qq.com>
2019-07-28 10:31:12 +0800

    update code

--- back/apis/sns/wx/main.js
+++ back/apis/sns/wx/main.js
@@ -5,19 +5,21 @@ class Main extends Api {
     constructor(who) {
         super(who)
     }
-    // 返回站点对应的appid
+    /**
+     * 返回站点或平台关联的微信公众号appid
+     */
     async appid(req) {
         const siteid = req.query.site
         const modelWx = new Config()
         try {
             let oWx
-            oWx = await modelWx.bySite(siteid, { fields: 'id,appid' })
-            if (!oWx) {
-                oWx = await modelWx.bySite('platform', { fields: 'id,appid' })
-                if (!oWx)
+            oWx = await modelWx.bySite(siteid, { fields: 'id,appid,joined' })
+            if (!oWx || oWx.joined !== 'Y') {
+                oWx = await modelWx.bySite('platform', { fields: 'id,appid,joined' })
+                if (!oWx || oWx.joined !== 'Y')
                     return {
                         code: 1,
-                        errmsg: '查找的对象不存在'
+                        errmsg: '没有获得可用数据'
                     }
             }
 
