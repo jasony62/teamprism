0b0147766f91936149ee3fd8689420860cac38d8
aYellowApple <2764411477@qq.com>
2019-08-15 15:08:08 +0800

    ddd

--- front/src/tms/components/List.vue
+++ front/src/tms/components/List.vue
@@ -1,38 +1,66 @@
 <template>
     <div id="tms-list">
-        <DynamicScroller
-            :items="items"
-            :min-item-size="54"
-            class="scroller"
+        <!-- <ul>
+            <li
+                v-for="item in items"
+                :key="item.id"
+            >
+                <slot
+                    name="item"
+                    :item="item"
+                ></slot>
+            </li>
+        </ul> -->
+        <van-list
+            v-model="loading"
+            :finished="finished"
+            finished-text="没有更多了"
+            @load="onLoad"
         >
-            <template v-slot="{ item, index, active }">
-                
+            <template v-slot:loading>
+                <van-loading type="spinner" size="20" />
             </template>
-        </DynamicScroller>
+            <van-cell
+                v-for="item in list"
+                :key="item"
+                :title="item"
+            />
+        </van-list>
     </div>
 </template>
 <script>
+import { List } from "vant";
 export default {
     props: {
         items: Array
+    },
+    components: {
+        [List.name]: List
+    },
+    data() {
+        return {
+            loading: false,
+            finished: false
+        };
+    },
+    methods: {
+        onLoad: function() {
+            // 异步更新数据
+            setTimeout(() => {
+                for (let i = 0; i < 10; i++) {
+                    this.items.push(this.items.length + 1);
+                }
+                // 加载状态结束
+                this.loading = false;
+
+                // 数据全部加载完成
+                if (this.items.length >= 40) {
+                    this.finished = true;
+                }
+            }, 500);
+        }
     }
 };
 </script>
 <style scoped>
-#tms-list,
-.scroller {
-    height: 100%;
-}
-
-#tms-list {
-    overflow: hidden;
-}
-
-.message {
-    display: flex;
-    min-height: 32px;
-    padding: 12px;
-    box-sizing: border-box;
-}
-
 </style>
--- front/src/ue/matter/enroll/common/RecordListItem.vue
+++ front/src/ue/matter/enroll/common/RecordListItem.vue
@@ -1,39 +1,15 @@
 <template>
     <div class="record">
-        <DynamicScrollerItem
-            :item="record"
-            :active="active"
-            :size-dependencies="[record.label]"
-            :data-index="index"
-            :data-active="active"
-            :title="`Click to change message ${index}`"
-            class="message"
-            @click.native="alertId(record)"
-        >
-            <div class="text">
-                {{ item.label }}
-            </div>
-        </DynamicScrollerItem>
+        <div>{{record.id}}</div>
+        <div>{{record.label}}</div> 
     </div>
 </template>
 
 <script>
 export default {
-    props: { record: Object },
-    methods: {
-        alertId(record) {
-            window.alert(record.id);
-        }
-    }
+    props: { record: Object }
 };
 </script>
 <style scoped>
-.index,
-.text {
-    flex: 1;
-}
 
-.text {
-    max-width: 400px;
-}
 </style>
