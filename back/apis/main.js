f037799488aafd34b17e0691da4d9cecd19a95f2
liao1a <153887111@qq.com>
2019-08-21 11:33:37 +0800

    11

--- back/apis/main.js
+++ back/apis/main.js
@@ -1,8 +1,8 @@
 const { Api, ResultData } = require('../tms/api')
 
 class Main extends Api {
-    constructor(args) {
-        super(args)
+    constructor(...args) {
+        super(...args)
     }
     version() {
         return new ResultData('0.1')
