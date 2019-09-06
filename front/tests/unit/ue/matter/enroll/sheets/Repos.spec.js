15b9828d834f0f016286c6c9a5b345db84cdc847
aYellowApple <2764411477@qq.com>
2019-09-06 10:24:28 +0800

    try add test

--- front/tests/unit/ue/matter/enroll/sheets/Repos.spec.js
+++ front/tests/unit/ue/matter/enroll/sheets/Repos.spec.js
@@ -1,23 +1,23 @@
+import { mount, createLocalVue } from "@vue/test-utils"
+import {Tab, Tabs} from "Vant"
 import ReposVue from "@/ue/matter/enroll/sheets/Repos"
-import { shallowMount } from "@vue/test-utils"
+
+const localVue = createLocalVue()
+localVue.use(Tabs).use(Tab)
 
 describe("ue", () => {
     describe("matter", () => {
         describe("enroll", () => {
             describe("sheets", () => {
                 describe("repos.js", () => {
-                    it("是否触发点击事件", ()=> {
-                        const wrapper = shallowMount(ReposVue, {
-                            propsData: {
-                                app: {title: '111'},
-                                user: {}
-                            }
-                        });
-                        wrapper.findAll(".van-tab").at(1).addClass("van-tab-active");
-                        expect(wrapper.vm.changeRouter).toHaveBeenCalledTimes(1);
-                    });
-                    it("改变后当前路由是否与activeName相匹配", ()=> {
+                    it("点击后，当前路由、activeName是否改变", ()=> {
+                        const wrapper = mount(ReposVue);
 
+                        //1、获得点击的按钮
+                        //2、触发点击事件
+                        //3、断言activeName是否与点击前不一致
+                        //4、断言路由是否已改变
+                        //5、断言路由是否与activeName相同
                     })
 
                 })
