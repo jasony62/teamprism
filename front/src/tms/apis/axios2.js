1a4de77a48e956d37e98a8674d984c6733105e7c
jasony62 <jasony62@qq.com>
2019-09-08 14:41:18 +0800

    优化axios2代码及错误处理

--- front/src/tms/apis/axios2.js
+++ front/src/tms/apis/axios2.js
@@ -1,5 +1,30 @@
 import axios from 'axios'
 
+const Axios2Error = (function() {
+    const _msg = new WeakMap()
+    class Axios2Error {
+        constructor(err) {
+            if (typeof err === 'string')
+                _msg.set(this, err)
+            else if (typeof err === 'object' && err.msg)
+                _msg.set(this, err.msg)
+            else
+                _msg.set(this.err.toString())
+        }
+        get msg() {
+            return `axios2:${_msg.get(this)}`
+        }
+    }
+    return Axios2Error
+})()
+
+class ResponseError {
+    constructor({ msg = '未知错误', code = 1 } = {}) {
+        this.msg = msg
+        this.code = code
+    }
+}
+
 class AccessToken {
     constructor(siteId) {
         this.siteId = siteId
@@ -27,18 +52,16 @@ class AccessToken {
     refresh() {
         let that = this
         let siteId = this.siteId
-        return new Promise((resolve, reject) => {
-            axios.get(`/ue/auth/token?site=${siteId}`).then((res) => {
+        return axios.get(`/ue/auth/token?site=${siteId}`)
+            .then((res) => {
                 let { access_token: accessToken, expire_in } = res.data.result
                 // 记录过期时间
                 let expireAt = parseInt(new Date / 1000) + expire_in - 120
                 // 保留获取的数据
                 that._store(`${accessToken}:${expireAt}`)
-                resolve(accessToken)
-            }).catch(err => {
-                reject(`axios2:${err}`)
+                return accessToken
             })
-        })
+            .catch(err => Promise.reject(new ResponseError(err)))
     }
 }
 /**
@@ -52,25 +75,21 @@ function useApiRequestInterceptor() {
             if (config) {
                 if (config.url.indexOf('/ue/api/') === 0) {
                     if (config.params === undefined) config.params = {}
-                    // 检查token是否已经过期，如果过期，重新获取token
                     let accessToken = myAccessToken.getCached()
                     if (accessToken) {
                         config.params.access_token = accessToken
                     } else {
-                        return new Promise((resolve, reject) => {
-                            myAccessToken.refresh().then(accessToken => {
+                        return myAccessToken.refresh()
+                            .then(accessToken => {
                                 config.params.access_token = accessToken
-                                resolve(config)
-                            }).catch(reject)
-                        })
+                                return config
+                            })
+                            .catch(err => Promise.reject(new Axios2Error(err)))
                     }
                 }
             }
             return config
-        }, error => {
-            // 对请求错误做些什么
-            return Promise.reject(error)
-        })
+        }, err => Promise.reject(err))
     }
 }
 
@@ -86,28 +105,23 @@ function useResponseInterceptor() {
                 switch (res.data.code) {
                     case 20001:
                         // 清除缓存的token
-                        myAccessToken._removeToken()
-                        // 如何重发request？
-                        return new Promise((resolve, reject) => {
-                            myAccessToken.refresh().then(() => {
-                                axios.request(res.config).then(res => {
-                                    resolve(res)
-                                }).catch(reject)
-                            }).catch(reject)
-                        })
+                        myAccessToken._remove()
+                        // 获得新的access_token，重发request
+                        return myAccessToken.refresh()
+                            .then(() => axios.request(res.config))
+                            .then(res => res)
+                            .catch(err => Promise.reject(new Axios2Error(err)))
                     default:
-                        return Promise.reject(res.data.msg)
+                        return Promise.reject(res.data)
                 }
             }
             return res
-        }, error => {
-            return Promise.reject(error)
-        })
+        }, err => Promise.reject(err))
     }
 }
 
 // 统一处理所有的响应
-if (!myResponseInterceptor) useResponseInterceptor()
+useResponseInterceptor()
 
 // 最后一次设置access_token时的siteId
 let myAccessToken
@@ -117,22 +131,17 @@ let myAccessToken
  * @param {String} siteid 
  */
 function setupAccessToken(siteid) {
-    if (!siteid) return Promise.reject('axios2:参数错误')
+    if (!siteid) return Promise.reject(new Axios2Error('参数错误'))
 
     myAccessToken = new AccessToken(siteid)
 
-    return new Promise((resolve, reject) => {
-        let accessToken = myAccessToken.getCached()
-        if (accessToken) {
-            if (!myApiRequestInterceptor) useApiRequestInterceptor()
-            resolve(axios)
-        } else {
-            myAccessToken.refresh().then(() => {
-                if (!myApiRequestInterceptor) useApiRequestInterceptor()
-                resolve(axios)
-            }).catch(err => { reject(`axios2:${err}`) })
-        }
-    })
+    return Promise.resolve(myAccessToken.getCached())
+        .then(accessToken => {
+            return accessToken ? accessToken : myAccessToken.refresh()
+        }).then(() => {
+            useApiRequestInterceptor()
+            return axios
+        }).catch(err => Promise.reject(new Axios2Error(err)))
 }
 
 export { setupAccessToken }
